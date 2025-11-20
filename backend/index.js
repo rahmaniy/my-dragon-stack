c97cc8f7b2fb5d74e9ac386286ba88f7ffc57e84
rahmaniy <nrahmaniyanti@gmail.com>
2025-11-20 15:26:03 +0700

    commit 201125

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,25 @@
-const Dragon = require('./dragon.js');
+const Dragon = require('./dragon');
 
 const fooey = new Dragon({
     birthdate: new Date(), 
     nickname: 'fooey'
 });
+
 const baloo = new Dragon({
     birthdate: new Date(), 
-    nickname: 'baloo'
+    nickname: 'baloo',
+    traits: [
+        { traitType: 'backgroundColor', traitValue: 'green'}
+    ]
 });
+
+const mimar = new Dragon();
+
+setTimeout(() => {
+    const gooby = new Dragon();
+    console.log('gooby', gooby);
+}, 3000);
 
 console.log('fooey', fooey);
 console.log('baloo', baloo);
+console.log('mimar', mimar);
